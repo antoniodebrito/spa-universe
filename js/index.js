bd999829284b3cb2600cf6db34b3734628afdc05
antoniodebrito <antonio11brito@gmail.com>
2022-11-02 09:22:47 -0300

    :sparkles: feat: created route calls

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import { Router } from "./routes";
+
+const router = new Router();
+router.add("/", "/pages/home.html");
+router.add("/", "/pages/universe.html.html");
+router.add("/", "/pages/exploration.html.html");
+router.add("/", "/pages/404.html.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
